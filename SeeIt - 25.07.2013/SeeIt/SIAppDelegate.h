7a2045d2d2a6407ea44c1d0df3dcfd8b433e8da3
Visionery <atanas.special@yahoo.com>
2013-07-25 19:33:21 +0300

    SeeIt 25.07.13
    Added weather
    new cover picture

---
+++ SeeIt - 25.07.2013/SeeIt/SIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SIAppDelegate.h
+//  SeeIt
+//
+//  Created by Atanas on 7/23/13.
+//  Copyright (c) 2013 Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SeeIt - 25.07.2013/SeeIt/menuSIViewController.h
@@ -0,0 +1,13 @@
+//
+//  menuSIViewController.h
+//  SeeIt
+//
+//  Created by Atanas on 7/23/13.
+//  Copyright (c) 2013 Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface menuSIViewController : UIViewController
+
+@end
